7528ccce74d83a7e32e9229a743c138b5f6abd02
AlekseyBorichevskiy <borichevskiy03@mail.ru>
2022-07-04 19:16:24 +0300

    first

---
+++ src/products/products.module.ts
@@ -0,0 +1,18 @@
+import { Module } from "@nestjs/common";
+import { ProductService } from "./products.service";
+import { ProductsController } from "./products.controller";
+import { MongooseModule } from "@nestjs/mongoose";
+import { Product, ProductSchema } from "./shemas/product.shema";
+
+@Module({
+  providers: [ProductService],
+  controllers: [ProductsController],
+  imports: [
+    MongooseModule.forFeature([
+      {name: Product.name, schema: ProductSchema}
+    ])
+  ]
+})
+export class ProductsModule {
+
+}
